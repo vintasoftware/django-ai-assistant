00f769147df76d553f8224dcfce23091267ba6c9
Flávio Juvenal <flavio@vinta.com.br>
2024-05-17 14:00:58 -0300

    Ruff fix all files

--- django_ai_assistant/urls.py
+++ django_ai_assistant/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
--- django_ai_assistant/wsgi.py
+++ django_ai_assistant/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_ai_assistant.settings')
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_ai_assistant.settings")
 
 application = get_wsgi_application()
