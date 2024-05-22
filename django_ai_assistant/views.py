f432bf2fc8614ce677494aa58ff7efab3c940dd5
Flávio Juvenal <flavio@vinta.com.br>
2024-05-22 17:07:02 -0300

    Message send and thread run integrated

--- django_ai_assistant/views.py
+++ django_ai_assistant/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.http import HttpRequest, JsonResponse
 from django.utils.module_loading import import_string
 from django.views import View
@@ -41,9 +43,18 @@ class AssistantThreadMessageListCreateView(AssistantViewMixin, View):
         async for message in client.beta.threads.messages.list(thread_id=self.kwargs["thread_id"]):
             yield message
 
-    async def create_thread_message(self):
+    async def create_thread_message(self, content: str):
         client = self.get_ai_client()
-        return await client.beta.threads.messages.create(thread_id=self.kwargs["thread_id"])
+        return await client.beta.threads.messages.create(
+            thread_id=self.kwargs["thread_id"], role="user", content=content
+        )
+
+    async def create_run(self):
+        client = self.get_ai_client()
+        # TODO: manage assistants
+        return await client.beta.threads.runs.create_and_poll(
+            thread_id=self.kwargs["thread_id"], assistant_id="asst_Hc05cQHz22rDpHFbkGV9eVEN"
+        )
 
     async def get(self, request: HttpRequest, *args, **kwargs):
         messages = []
@@ -55,6 +66,19 @@ class AssistantThreadMessageListCreateView(AssistantViewMixin, View):
     async def post(self, request: HttpRequest, *args, **kwargs):
         if not self.can_create_message():
             return JsonResponse({"error": "User is not allowed to create messages"}, status=403)
+        try:
+            body_data = json.loads(request.body)
+        except json.JSONDecodeError:
+            return JsonResponse({"error": "Invalid JSON data"}, status=400)
+        if not body_data.get("content"):
+            return JsonResponse({"error": "Message content is required"}, status=400)
 
-        message = await self.create_thread_message()
-        return JsonResponse(message.to_dict())
+        message = await self.create_thread_message(content=body_data["content"])
+        run = await self.create_run()
+
+        return JsonResponse(
+            {
+                "message": message.to_dict(),
+                "run_id": run.to_dict(),
+            }
+        )
--- example/assets/js/Chat/Chat.tsx
+++ example/assets/js/Chat/Chat.tsx
@@ -8,16 +8,37 @@ import {
   Group,
   Box,
   Flex,
+  ActionIcon,
+  Button,
+  Code,
+  LoadingOverlay,
+  ScrollArea,
 } from "@mantine/core";
 import { ThreadsNav } from "./ThreadsNav";
 import OpenAI from "openai";
+import cookie from "cookie";
 
 import classes from "./Chat.module.css";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { IconSend2 } from "@tabler/icons-react";
+import { getHotkeyHandler } from "@mantine/hooks";
+
+function csrfFetch(url: string, options: RequestInit = {}) {
+  const { csrftoken } = cookie.parse(document.cookie);
+
+  return fetch(url, {
+    ...options,
+    headers: {
+      ...options.headers,
+      "X-CSRFToken": csrftoken,
+    },
+    credentials: "include",
+  });
+}
 
 function ChatMessage({ message }: { message: OpenAI.Beta.Threads.Message }) {
   return (
-    <Box className={classes.chatMessage} mb="md">
+    <Box mb="md">
       <Text fw={700} style={{ textTransform: "capitalize" }}>
         {message.role}
       </Text>
@@ -28,11 +49,7 @@ function ChatMessage({ message }: { message: OpenAI.Beta.Threads.Message }) {
         } else {
           text = content.text.value;
         }
-        return (
-          <Text key={index} mt="sm">
-            {text}
-          </Text>
-        );
+        return <Text key={index}>{text}</Text>;
       })}
     </Box>
   );
@@ -52,23 +69,77 @@ function ChatMessageList({
   );
 }
 
-async function getMessages({ threadId }: { threadId: string }) {
-  const response = await fetch(`/ai-assistant/threads/${threadId}/messages`);
+async function fetchMessages({ threadId }: { threadId: string }) {
+  const response = await csrfFetch(
+    `/ai-assistant/threads/${threadId}/messages/`
+  );
   return response.json();
 }
 
+async function createMessage({
+  threadId,
+  content,
+}: {
+  threadId: string;
+  content: string;
+}) {
+  await csrfFetch(`/ai-assistant/threads/${threadId}/messages/`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ content }),
+  });
+}
+
 export function Chat() {
-  const threadId = "thread_s5Dxm6QTPhjOhA0ZFqZ6HXf2";
+  const threadId = "thread_95qH0BqgjbvBYUTiBttGLEum";
 
+  const [inputValue, setInputValue] = useState<string>("");
   const [messages, setMessages] = useState<OpenAI.Beta.Threads.Message[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  useEffect(() => {
-    getMessages({ threadId }).then((response) => {
+  const scrollViewport = useRef<HTMLDivElement>(null);
+  const scrollToBottom = useCallback(
+    () =>
+      scrollViewport.current?.scrollTo({
+        top: scrollViewport.current!.scrollHeight,
+        behavior: "smooth",
+      }),
+    [scrollViewport]
+  );
+
+  const loadMessages = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetchMessages({ threadId });
       const messages = response.data as OpenAI.Beta.Threads.Message[];
       messages.reverse();
       setMessages(messages);
-    });
-  });
+      scrollToBottom();
+    } catch (error) {
+      console.error(error);
+      alert("Error while loading messages");
+    }
+    setIsLoading(false);
+  }, [threadId]);
+
+  const sendMessage = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      setInputValue("");
+      await createMessage({ threadId, content: inputValue });
+      await loadMessages();
+    } catch (error) {
+      console.error(error);
+      alert("Error while sending message");
+    }
+    setIsLoading(false);
+  }, [threadId, inputValue]);
+
+  useEffect(() => {
+    loadMessages();
+  }, [loadMessages]);
 
   return (
     <>
@@ -79,10 +150,48 @@ export function Chat() {
             <Title mt="md" order={2}>
               Chat
             </Title>
-            <Stack className={classes.chatMessages}>
+            <ScrollArea
+              pos="relative"
+              type="auto"
+              h="100%"
+              px="xs"
+              viewportRef={scrollViewport}
+            >
+              <LoadingOverlay
+                visible={isLoading}
+                zIndex={1000}
+                overlayProps={{ blur: 2 }}
+              />
               <ChatMessageList messages={messages} />
-            </Stack>
-            <Textarea mt="auto" mb="3rem" placeholder="Enter user message…" />
+            </ScrollArea>
+            <Textarea
+              mt="auto"
+              mb="3rem"
+              placeholder="Enter user message… (Ctrl↵ to send)"
+              autosize
+              minRows={2}
+              onChange={(e) => setInputValue(e.currentTarget.value)}
+              value={inputValue}
+              onKeyDown={getHotkeyHandler([["mod+Enter", sendMessage]])}
+              rightSection={
+                <Button
+                  variant="filled"
+                  color="teal"
+                  aria-label="Send message"
+                  fz="xs"
+                  rightSection={
+                    <IconSend2
+                      stroke={1.5}
+                      style={{ width: "70%", height: "70%" }}
+                    />
+                  }
+                  onClick={sendMessage}
+                >
+                  Send
+                </Button>
+              }
+              rightSectionWidth={120}
+            />
           </Stack>
         </Container>
       </main>
