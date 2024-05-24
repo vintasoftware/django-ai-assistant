d62676abd3933b2b77fccc62dfd66615b3a3771d
Flávio Juvenal <flavio@vinta.com.br>
2024-05-24 15:40:04 -0300

    WEATHER_API_KEY info

--- example/example/settings.py
+++ example/example/settings.py
@@ -139,4 +139,4 @@ AI_ASSISTANT_CAN_CREATE_THREAD_FN = "django_ai_assistant.permissions.allow_all"
 AI_ASSISTANT_CAN_VIEW_THREAD_FN = "django_ai_assistant.permissions.allow_all"
 AI_ASSISTANT_CAN_CREATE_MESSAGE_FN = "django_ai_assistant.permissions.allow_all"
 AI_ASSISTANT_CAN_USE_ASSISTANT = "django_ai_assistant.permissions.allow_all"
-WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
+WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")  # get for free at https://www.weatherapi.com/
