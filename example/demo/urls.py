99d1469a4fcbe65fe39dd4639438f7cd85c1d40b
Pamella Bezerra <pamella@vinta.com.br>
2024-05-28 11:38:13 -0300

    WIP - HTMX

--- example/demo/urls.py
+++ example/demo/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path("ai-assistant/", include("django_ai_assistant.urls")),
     path("", views.index, name="index"),
+    path("htmx-chat/", views.htmx_chat, name="htmlx_chat"),
 ]
--- example/demo/views.py
+++ example/demo/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "demo/index.html")
+
+
+def htmx_chat(request):
+    return render(request, "demo/htmx_chat.html")
