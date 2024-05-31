e95cfa356b97fd654dd8db34f3b50352603969d3
Flávio Juvenal <flavio@vinta.com.br>
2024-05-31 16:40:46 -0300

    HTMX without JS

--- example/demo/urls.py
+++ example/demo/urls.py
@@ -5,7 +5,11 @@ from . import views
 
 urlpatterns = [
     path("ai-assistant/", include("django_ai_assistant.urls")),
+    path("htmx/", views.AIAssistantChatHomeView.as_view(), name="chat_home"),
+    path(
+        "htmx/thread/<int:thread_id>/",
+        views.AIAssistantChatThreadView.as_view(),
+        name="chat_thread",
+    ),
     path("", views.react_index, name="react_index"),
-    path("htmx/", views.htmx_index, name="htmx_index"),
-    path("htmx/thread/<int:thread_id>/", views.htmx_index, name="htmx_thread_detail"),
 ]
