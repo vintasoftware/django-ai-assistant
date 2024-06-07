56397840fe11fbd20fb137025a4ec6b04d77fb01
Pamella Bezerra <pamella@vinta.com.br>
2024-06-07 10:31:44 -0300

    Add comment

--- example/assets/js/App.tsx
+++ example/assets/js/App.tsx
@@ -1,12 +1,13 @@
 import "@mantine/core/styles.css";
 
-import React from "react";
 import { createTheme, MantineProvider } from "@mantine/core";
 import { Chat } from "@/Chat";
 import { configAiAssistant } from "django-ai-assistant-client";
 
 const theme = createTheme({});
 
+// Relates to path("ai-assistant/", include("django_ai_assistant.urls"))
+// which can be found at example/demo/urls.py)
 configAiAssistant("ai-assistant");
 
 const App = () => {
