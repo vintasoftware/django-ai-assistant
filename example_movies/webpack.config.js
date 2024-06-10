abaa1608914d724faeb1f58034ad4ff9392bd4cb
Pamella Bezerra <pamella@vinta.com.br>
2024-06-10 15:52:07 -0300

    Apply suggestions from code review
    Co-authored-by: Flávio Juvenal <flavio@vinta.com.br>

--- example_movies/webpack.config.js
+++ example_movies/webpack.config.js
@@ -61,6 +61,7 @@ module.exports = {
     extensions: [".js", ".ts", ".jsx", ".tsx"],
     alias: {
       "@": path.resolve(__dirname, "assets/js"),
+      // Necessary to deduplicate React due to npm link:
       react: path.resolve("./node_modules/react"),
     },
   },
