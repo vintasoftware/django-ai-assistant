dcc23059e29bcb8f1083244645a6aceee18c98c6
Pamella Bezerra <pamella@vinta.com.br>
2024-06-10 16:16:34 -0300

    Remove callbacks from hooks

--- frontend/src/hooks/useAssistant.ts
+++ frontend/src/hooks/useAssistant.ts
@@ -4,7 +4,6 @@ import {
   AssistantSchema,
   djangoAiAssistantViewsListAssistants,
 } from "../client";
-import { Callbacks } from "./types";
 
 /**
  * React hook to manage the Assistant resource.
@@ -17,25 +16,18 @@ export function useAssistant() {
   /**
    * Fetches a list of AI assistants.
    *
-   * @param onSuccess Callback function called upon successful fetching of assistants.
-   * @param onError Callback function called upon error while fetching assistants.
+   * @returns A promise that resolves with the fetched list of AI assistants.
    */
-  const fetchAssistants = useCallback(
-    async ({ onSuccess, onError }: Callbacks = {}) => {
-      try {
-        setLoadingFetchAssistants(true);
-        const fetchedAssistants = await djangoAiAssistantViewsListAssistants();
-        setAssistants(fetchedAssistants);
-        onSuccess?.();
-      } catch (error) {
-        console.error(error);
-        onError?.(error);
-      } finally {
-        setLoadingFetchAssistants(false);
-      }
-    },
-    []
-  );
+  const fetchAssistants = useCallback(async (): Promise<AssistantSchema[]> => {
+    try {
+      setLoadingFetchAssistants(true);
+      const fetchedAssistants = await djangoAiAssistantViewsListAssistants();
+      setAssistants(fetchedAssistants);
+      return fetchedAssistants;
+    } finally {
+      setLoadingFetchAssistants(false);
+    }
+  }, []);
 
   return {
     /**
