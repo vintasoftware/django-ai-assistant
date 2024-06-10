62a7f21297834dbf75a5651142814f639411b308
Pamella Bezerra <pamella@vinta.com.br>
2024-06-10 12:00:31 -0300

    Refactor for clarity

--- example/assets/js/App.tsx
+++ example/assets/js/App.tsx
@@ -2,13 +2,13 @@ import "@mantine/core/styles.css";
 
 import { createTheme, MantineProvider } from "@mantine/core";
 import { Chat } from "@/Chat";
-import { configAiAssistant } from "django-ai-assistant-client";
+import { configAIAssistant } from "django-ai-assistant-client";
 
 const theme = createTheme({});
 
 // Relates to path("ai-assistant/", include("django_ai_assistant.urls"))
 // which can be found at example/demo/urls.py)
-configAiAssistant("ai-assistant");
+configAIAssistant({ baseURL: "ai-assistant" });
 
 const App = () => {
   return (
