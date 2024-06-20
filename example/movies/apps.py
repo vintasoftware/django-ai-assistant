eecec62c813671dc4e81b5e65de2a5fa0fae5666
amandasavluchinske <amandasavluchinske@gmail.com>
2024-06-20 17:45:19 +0100

    Changes example/movies models to use UUIDs by default

--- example/movies/apps.py
+++ example/movies/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class MoviesConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
+    default_auto_field = "example.fields.UUIDAutoField"
     name = "movies"
