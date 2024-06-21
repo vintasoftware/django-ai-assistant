0e55aa101d193fbcf6b418938d6220eb425cca42
amandasavluchinske <amandasavluchinske@gmail.com>
2024-06-21 18:02:30 +0100

    Transforms cast_id into decorator

--- django_ai_assistant/helpers/formatters.py
+++ django_ai_assistant/helpers/formatters.py
@@ -1,7 +1,7 @@
 import uuid
 
 
-def format_id(item_id, model):
+def cast_id(item_id, model):
     if isinstance(item_id, str) and "UUID" in model._meta.pk.get_internal_type():
         return uuid.UUID(item_id)
     return item_id
