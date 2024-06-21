59db142477869515c4f6fa30f46fa0e6b9b9dce4
amandasavluchinske <amandasavluchinske@gmail.com>
2024-06-21 16:58:27 +0100

    Fixes formatter

--- django_ai_assistant/helpers/formatters.py
+++ django_ai_assistant/helpers/formatters.py
@@ -2,6 +2,6 @@ import uuid
 
 
 def format_id(item_id, model):
-    if isinstance(id, str) and "UUID" in model._meta.pk.get_internal_type():
+    if isinstance(item_id, str) and "UUID" in model._meta.pk.get_internal_type():
         return uuid.UUID(item_id)
     return item_id
