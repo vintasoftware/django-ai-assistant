9acccadc97bac71ff5ad224fd0338d4873ffce60
amandasavluchinske <amandasavluchinske@gmail.com>
2024-06-21 20:41:53 +0100

    Moves cast_id to decorators file

--- django_ai_assistant/decorators.py
+++ django_ai_assistant/decorators.py
@@ -1,8 +1,14 @@
+import uuid
 from functools import wraps
 
-from django_ai_assistant.helpers.formatters import cast_id
+
+def _cast_id(item_id, model):
+    if isinstance(item_id, str) and "UUID" in model._meta.pk.get_internal_type():
+        return uuid.UUID(item_id)
+    return item_id
 
 
+# Decorator to cast ids to the correct type when using workaround UUIDAutoField
 def with_cast_id(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
@@ -13,15 +19,15 @@ def with_cast_id(func):
         message_ids = kwargs.get("message_ids")
 
         if thread_id:
-            thread_id = cast_id(thread_id, Thread)
+            thread_id = _cast_id(thread_id, Thread)
             kwargs["thread_id"] = thread_id
 
         if message_id:
-            message_id = cast_id(message_id, Message)
+            message_id = _cast_id(message_id, Message)
             kwargs["message_id"] = message_id
 
         if message_ids:
-            message_ids = [cast_id(message_id, Message) for message_id in message_ids]
+            message_ids = [_cast_id(message_id, Message) for message_id in message_ids]
             kwargs["message_ids"] = message_ids
 
         return func(*args, **kwargs)
